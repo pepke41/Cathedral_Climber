b086b18a2f731f5aed9a6656f8686fda4258379d
Emily Pepke <elpepke41@gmail.com>
2017-02-10 19:01:00 -0800

    levels complete

--- climber_main.py
+++ climber_main.py
@@ -26,3 +26,15 @@ while level_index != -1 and level_index != 'end' and valid_match:
         if choice.lower() == i.choice_descript:
             level_index = i.next_level
             valid_match = True
+
+if valid_match == false or level_index == -1:
+    print('GAME OVER')
+else:
+    #if envelope give hint
+    print('This is it. The final battle. You see a jail cell, inside are all the defencers. \"Help us!!\" You wander over to the cage and see that there is a screen with a final riddle. If you answer correctly, you will release the defenders and save the day, but if you fail, the cell will self destruct. No pressure')
+    print('It is in the rock, but not in the stone; It is in the marrow, but not in the bone; It is in the bolster, but not in the bed; It is not in the living, nor yet in the dead. Answer with one word only')
+    choice = raw_input()
+    if choice.lower() == 'r':
+        print('The door unlocks, you\'ve saved the day!!!')
+    else:
+        print('GAME OVER')
